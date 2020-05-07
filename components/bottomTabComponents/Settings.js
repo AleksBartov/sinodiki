32cd63157bb8a4e1ad6e976edc957746c9b07510
AleksBartov <aleksandrbartov>
2020-05-07 13:04:28 +0300

    testing redash for cards moving

--- components/bottomTabComponents/Settings.js
+++ components/bottomTabComponents/Settings.js
@@ -1,62 +1,37 @@
 import React from 'react';
-import { View, SafeAreaView, TouchableHighlight, StyleSheet } from 'react-native';
-import Animated, { Value, set, block, eq, add, sub, event, cond, useCode } from 'react-native-reanimated';
-import { PanGestureHandler, State } from 'react-native-gesture-handler';
-import { timing } from 'react-native-redash';
+import { SafeAreaView, TouchableHighlight, Dimensions } from 'react-native';
+import Animated from 'react-native-reanimated';
 import { COLORS } from '../../constants/colors';
 import { Ionicons } from '@expo/vector-icons';
+import Card from './cardsForSettings/Card';
 
-const styles = StyleSheet.create({})
+const { width, height } = Dimensions.get('window')
+
+const CARD_SIZE = width - 100;
+
+const cards = [
+  {
+    title: 'о упокоении',
+    backgroundColor: COLORS.lightest,
+    color: COLORS.darkest,
+  },
+  {
+    title: 'о здравии',
+    backgroundColor: COLORS.darkest,
+    color: COLORS.lightest,
+  },
+]
+
+const offsets = cards.map((_, index) => {
+  return (
+    {
+      x: new Animated.Value(index === 0 ? 60 : 0),
+      y: new Animated.Value(index === 0 ? 60 : 0),
+    }
+  )
+})
 
 export default function Settings ({ navigation, route }) {
-    
-  const gestureState = new Value(State.UNDETERMINED);
-  const dragX = new Value(0);
-  const dragY = new Value(0);
-  const transX = new Value(0);
-  const transY = new Value(0);
-
-  const gestureHandler = event([
-    {
-      nativeEvent: {
-        state: gestureState,
-        translationY: dragY,
-        translationX: dragX,
-      },
-    },
-  ]);
-
-  const prevDragX = new Value(0);
-  const prevDragY = new Value(0);
-
-  const _transY = block([
-    cond(
-      eq(gestureState, State.ACTIVE),
-      [
-        set(transY, add(transY, sub(dragY, prevDragY))),
-        set(prevDragY, dragY),
-      ],
-      set(prevDragY, 0)
-    ),
-    transY,
-  ]);
-
-  const _transX = block([
-    cond(
-      eq(gestureState, State.ACTIVE),
-      [
-        set(transX, add(transX, sub(dragX, prevDragX))),
-        set(prevDragX, dragX),
-      ],
-      set(prevDragX, 0)
-    ),
-    transX,
-  ]);
-
-    const scale = cond(
-        eq(gestureState, State.BEGAN),
-        timing({ from: 1, to: 1.1, duration: 1550 }),
-        cond(eq(gestureState, State.ACTIVE), 1.1, 1));
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.middle, alignItems: 'center', justifyContent: 'center' }}>
@@ -65,25 +40,13 @@ export default function Settings ({ navigation, route }) {
             onPress={() => navigation.navigate('Home')}>
             <Ionicons name="ios-arrow-round-back" size={34} color={COLORS.deepBlue} />
           </TouchableHighlight>
-
-          <PanGestureHandler onGestureEvent={gestureHandler} onHandlerStateChange={gestureHandler}>
-            <Animated.View
-                style={{ 
-                    width: 200,
-                    height: 200,
-                    borderWidth: 1,
-                    transform: [
-                        {
-                            translateX: _transX
-                        },
-                        {
-                            translateY: _transY
-                        },
-                        { scale: scale }
-                    ] }}>
-            </Animated.View>
-          </PanGestureHandler>
-
+           {
+             cards.map((card, index) => {
+               return (
+                 <Card key={index} {...{ card, index, offsets }} />
+               )
+             })
+           }
         </SafeAreaView>
     )
   }
---
+++ components/bottomTabComponents/cardsForSettings/Card.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Text, Dimensions } from 'react-native';
+import { COLORS } from '../../../constants/colors';
+import Animated, { add } from 'react-native-reanimated';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+import { panGestureHandler } from 'react-native-redash';
+
+const { width, height } = Dimensions.get('window')
+
+const CARD_SIZE = width - 100;
+
+export default function Card ({ card, index, offsets }) {
+    const {
+        gestureHandler,
+        state,
+        translationX,
+        translationY
+    } = panGestureHandler();
+
+    const translateX = add(offsets[index].x, translationX);
+
+    const translateY = add(offsets[index].y, translationY);
+  
+    return (
+        <PanGestureHandler {...gestureHandler} >
+            <Animated.View 
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    marginTop: CARD_SIZE - 50,
+                    marginLeft: width/2-CARD_SIZE/2,
+                    width: CARD_SIZE,
+                    height: CARD_SIZE,
+                    borderWidth: 2,
+                    borderRadius: 10,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: card.backgroundColor,
+                    transform: [
+                        { translateX },
+                        { translateY },
+                    ]
+                }} >
+                <Text style={{ color: card.color }}>{ card.title }</Text>
+            </Animated.View>
+        </PanGestureHandler>
+    )
+  }
